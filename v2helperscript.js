9bd1adad1ea847688c19f64e29d06abec31802bf
vampycat237 <98717915+vampycat237@users.noreply.github.com>
2022-08-14 15:03:14 -0600

    beginnings of v2
    - all changes to index.html are basically just to keep it looking mostly the same.
    - only ACTUAL change to v1 is that it's slightly wider now
    
    as for v2... it'll be a more free-form thing than v1 (which is very linear) and hopefully be more ideal for multiple bay pieces.
    - created v2.html and its own helper script
    - v2 is planned to have small draggable and maybe collapsible windows that will kinda do their own things, and the calculate button will base its calcs on all that other stuff
    - v2 will be built with multi-bay, multi-media, etc pieces as its main focus. single-bay pieces will probably make more sense for v1

---
+++ v2helperscript.js
@@ -0,0 +1,53 @@
+//helper methods for v2 stuff!
+
+//toggles the class "selected" on a dropdown container
+function toggleSelected(divId) {
+	//retrieves the dropdown container element
+	const dropdown = document.getElementById(divId);
+	dropdown.classList.toggle("selected");
+}
+
+//code to make draggable windows - taken from w3schools.com! too many things for my brain.
+//Make the DIV element draggagle:
+dragElement(document.getElementById("media"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
