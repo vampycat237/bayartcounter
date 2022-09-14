44b07f20719cd0e8592955abebbae13601bf2a9a
vampycat237 <98717915+vampycat237@users.noreply.github.com>
2022-09-14 16:18:05 -0600

    sidebar, base value editor, minor v1 style tweaks
    - added a sidebar to open and close draggable windows
    - collapsed media and coverage (craftSize soon to follow) into 'base values'
    - started work on base value editor code so it will actually do something
    
    i will be re-organizing the file system next commit, it's time
    - made more windows draggable (using new dummy class 'draggable' and loops) but also hid more windows by default
    - replaced specialized methods showDebug and hideDebug with generic methods show and hide, which toggleDebug now uses instead
    - added generic functions showHide and toggleVisibility for showing and hiding elements that together can handle both IDs and dom elements
    - removed some commented out code

--- v2helperscript.js
+++ v2helperscript.js
@@ -7,16 +7,19 @@ function toggleSelected(divId) {
 	dropdown.classList.toggle("selected");
 }
 
-//code to make draggable windows - taken from w3schools.com! too many things for my brain.
-//Make the DIV element draggagle:
-dragElement(document.getElementById("media"));
-dragElement(document.getElementById("debug"));
+//code to make draggable windows - taken from w3schools.com & modified! too many things for my brain.
+//List of elements to make draggable
+const draggables = document.getElementsByClassName("draggable");
+//Make the DIV elements draggable:
+for (var e of draggables) {
+	dragElement(e);
+}
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  if (document.getElementById(elmnt.id + "-header")) {
     /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
   } else {
     /* otherwise, move the DIV from anywhere inside the DIV:*/
     elmnt.onmousedown = dragMouseDown;
@@ -55,16 +58,12 @@ function dragElement(elmnt) {
 
 //message management
 const messageContainer = document.getElementById('message-container');
-//const messageBorder = document.getElementById('message-border');
-//const messageDiv    = document.getElementById('message');
 const messageText   = document.getElementById('message-content');
-//const messageButton = document.getElementById('message-button');
 
 function showMessage(message, timeToShow = 5000) {
 	messageContainer.style.display = "flex";
 	
 	messageText.innerHTML   = message;
-	//toggleMessageVisibility();
 	messageContainer.classList.add("selected");
 	
 	//setTimeout(hideMessage, timeToShow);
@@ -78,18 +77,6 @@ function hideMessage() {
 	setTimeout(hideMessage2, 3000);
 }
 
-//helper method to reduce redundancy
-/*function toggleMessageVisibility() {
-	//old way - fades in and out with transparency
-	/*messageBorder.classList.toggle("show");
-	messageDiv.classList.toggle("show");
-	messageText.classList.toggle("show");
-	messageButton.classList.toggle("show");*/
-	
-	/*//new way - expands down like the dropdowns, using the same code
-	toggleSelected('message-container');
-}*/
-
 function hideMessage2() {
 	messageContainer.style.display = "none";
 }
@@ -99,16 +86,137 @@ const debugMenu = document.getElementById('debug');
 
 function toggleDebug() {
 	if (debugMenu.style.display == "none") {
-		showDebug();
+		show(debugMenu);
 	} else {
-		hideDebug();
+		hide(debugMenu);
 	}
 }
 
-function showDebug() {
-	debugMenu.style.display = "flex";
+//elmntId version of toggleVisibility
+function showHide(elmntId) {
+	toggleVisibility(document.getElementById(elmntId))
 }
 
-function hideDebug() {
-	debugMenu.style.display = "none";
+function toggleVisibility(elmnt, secondCall = false) {
+	//if elmnt is a string, we try to call again thru showHide() assuming it's an ID
+	if (typeof elmnt == 'string') {
+		showHide(elmnt);
+		return;
+	}
+	
+	//elmnt is an element! phew.
+	else {
+		if (elmnt.style.display == "none") {
+			show(elmnt);
+		}
+		else {
+			hide(elmnt);
+		}
+	}
+}
+
+function hide(elmnt) {
+	elmnt.style.display = "none";
+}
+
+function show(elmnt, d = "flex") {
+	elmnt.style.display = d;
+}
+
+//base value editing
+//TODO: store base values in here
+var baseValues = [];
+//base values editor
+const baseValEditor = {
+	editor   : document.getElementById('base-values'),
+	header   : document.getElementById('base-values-header'),
+	count    : document.getElementById('base-values-count'),
+	media    : document.getElementById('base-values-media'),
+	coverage : document.getElementById('base-values-coverage'),
+	action   : document.getElementById('base-values-action'),
+	index    : document.getElementById('base-values-index')
+};
+
+//TODO: shows BaseVal editor while making its title change to fit the action being performed
+//parameter action: a string. either "edit" or "new"/"create"/"add"
+//parameter i: an int, meant to be the BaseVal's index
+function openBaseValEditor(action, i = -1) {
+	var title = "base value editor";
+	var count = 1;
+	var media = "flatcolor";
+	var coverage = "fullbody";
+	
+	//calls actionIsEdit() to check for valid edit requests. if it's valid for editing it returns true, otherwise it returns false. it also prints an error when there is an invalid action passed in.
+	if (actionIsEdit(action, i)) {
+		console.log("opening base value editor: edit");
+		
+		//set title & set values to the stuff from the object we are editing
+		title = "edit base value";
+		
+		//TODO: get info for these
+		count = 1;
+		media = "flatcolor";
+		coverage = "fullbody";
+		
+		//TODO: fetch BaseVal's content to display for editing
+		
+	} else {
+		console.log("opening base value editor: add new");
+		
+		//set title, leave other stuff as default
+		title = "add new base value";
+	}
+	
+	//now we are clear to update the editor's into then show the editor
+	//edit the innerHTML
+	baseValEditor.header.innerHTML = title;
+	baseValEditor.count.innerHTML  = count;
+	baseValEditor.media.innerHTML  = media;
+	baseValEditor.coverage.innerHTML = coverage;
+	
+	//set the hidden values
+	baseValEditor.action.value = action;
+	baseValEditor.index.value  = i;
+	
+	//show the editor
+	show(baseValEditor.editor);
+	
+}
+
+//TODO: Upon confirming edit BaseVal, edit the BaseVal based on index
+//paramether value: values to pass into the object
+//parameter i: index in the baseValues list
+function editBaseVal(value, i) {
+	//TODO: construct baseval from values (or pass object as parameter?)
+	
+	//TODO: find and replace the old baseval with the new one
+	
+	console.log("edited base val [non-functional]");
+}
+
+//TODO: Upon confirming add BaseVal, add the new BaseVal
+//paramether value: values to pass into the object
+function addBaseVal(value) {
+	//TODO: construct baseval from values (or pass object as parameter?)
+	
+	//TODO: add the new baseval to the end of the list
+	
+	console.log("added new base val [non-functional]");
+}
+
+//checks action! returns true for edit, false for anything else, and prints an error if the action is invalid
+//parameter: string action
+//returns: bool, based on if action == edit
+function actionIsEdit(action, i = -1) {
+	//case: edit
+	//TODO: add check for invalid index (greater than list size)
+	if (action == "edit" && i >= 0) { return true; }
+	
+	//case: create new
+	if (action == "new" || action == "create" || action == "add") { return false; }
+	
+	//case: invalid action
+	console.log("invalid action passed to base value editor. defaulting to adding new value");
+	showMessage("invalid action passed to base value editor.<br>defaulting to adding new value");
+	return false;
 }
