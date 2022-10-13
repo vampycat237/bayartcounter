ddc4e76dc660c4a41464744cfb34c9424fad9afe
vampycat237 <98717915+vampycat237@users.noreply.github.com>
2022-10-13 13:03:41 -0600

    started adding lineart to v2 & more
    - all inputs will use our nice little input styles now
    - removed unused styles from v2style
    - started work on creating a generic form of updateBVHolder()
    - added lineart windows and sidebar button, no functionality yet

--- js/v2/sidebar-manager.js
+++ js/v2/sidebar-manager.js
@@ -25,7 +25,7 @@ function updateBVHolder() {
 	for (i = 0; i < baseValues.length; i++) {
 	//this is the template to be filled out with the proper info basically.
 		bvContents[i] = [
-			'<div class="row button base-value-item" onclick="openBaseValEditor(\'edit\', '+i+')">',
+			'<div class="row button section-item" onclick="openBaseValEditor(\'edit\', '+i+')">',
 			'<span id="base-value-item-'+i+'">edit '+baseValues[i].toString()+'</span>',
 			'<button class="small red" onclick="removeBaseVal(0)">x</button>',
 			'</div>'
@@ -36,5 +36,24 @@ function updateBVHolder() {
 		sidebar.holders.BV.innerHTML = bvContents.join(" ");
 	} else {
 		sidebar.holders.BV.innerHTML = "";
+	}
+}
+
+function updateSectionHolder(section, list, func) {
+	const sectionContents = [];
+	for (i = 0; i < list.length; i++) {
+	//this is the template to be filled out with the proper info basically.
+		sectionContents[i] = [
+			'<div class="row button section-item" onclick="openBaseValEditor(\'edit\', '+i+')">',
+			'<span id="base-value-item-'+i+'">edit '+baseValues[i].toString()+'</span>',
+			'<button class="small red" onclick="removeBaseVal(0)">x</button>',
+			'</div>'
+		].join(" ");
+	}
+	
+	if (bvContents.length > 0) {
+		section.innerHTML = bvContents.join(" ");
+	} else {
+		section.innerHTML = "";
 	}
 }
